12d63eac169bdf9780291393a45a79dd2e270f23
blenux <blenux@outlook.com>
2025-11-09 20:15:35 +1100

    Revert version to 5.0 beta

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
-#include <stdbool.h>
 #include <stddef.h>
 
 /** \file
@@ -18,17 +17,17 @@
  */
 
 /** Blender major and minor version. */
-#define BLENDER_VERSION 501
+#define BLENDER_VERSION 500
 /** Blender patch version for bug-fix releases. */
 #define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE alpha
+#define BLENDER_VERSION_CYCLE beta
 /** Blender release type suffix. LTS or blank. */
 #define BLENDER_VERSION_SUFFIX
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 4
+#define BLENDER_FILE_SUBVERSION 117
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to
